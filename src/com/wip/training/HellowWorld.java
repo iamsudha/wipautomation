93106283ce86018a8788b612ccb4425728f2bb9f
Ananda Geetha Sudha Katta <kattaa@kattaa-macbookair.roam.corp.google.com>
2017-02-04 11:44:58 -0800

    first wip automation program push

---
+++ src/com/wip/training/HellowWorld.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.wip.training;
+
+/**
+ * @author kattaa
+ *
+ */
+public class HellowWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 25;
+		float price =20.99F;
+		int months=12;
+		char letter = 'a';
+		boolean isitem_damage;
+		int books =2;
+		boolean isfood_type ;
+		float pizzacost =6.675F;
+		String day = "Sunday";
+		int apps =10;
+		int pages = 10;
+		int week = 7;
+		double pizslc =  7.86d;
+		char country = 'I';
+		
+		
+		String name = "Sudha";
+		//String country = "India";
+		System.out.println("Week has" + week + "days");
+		System.out.println("Each pizza slice is" +pizslc+ "dollars");
+		System.out.println("First letter of INdia is" + country);
+		System.out.println("My name is" + name);
+		System.out.println("length of my name is " + name.length() + " letters");;
+		System.out.println("The cost of the pizza is "+ pizzacost);
+		System.out.println(apps + " apps installed in my phone");
+		System.out.println("So far I read " + pages +" pages");
+		
+		
+		
+				
+    System.out.println("Hello World");
+	}
+
+}
