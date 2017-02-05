e1aef0638ea574a6d6c9b153f52d7b08d44318fc
Ananda Geetha Sudha Katta <kattaa@kattaa-macbookair.roam.corp.google.com>
2017-02-04 18:25:00 -0800

    adding tutorial1 scripts

---
+++ src/com/wip/training/math.java
@@ -0,0 +1,16 @@
+package com.wip.training;
+
+public class math {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 4;
+int y = 2;
+
+System.out.println("(x+y)(x+y) is " +  (x+y)*(x+y));
+System.out.println("x%y is " +  x%2);
+System.out.println("2x+y is " +  (2*x+y));
+
+	}
+
+}
---
+++ src/com/wip/training/sumof100.java
@@ -0,0 +1,17 @@
+package com.wip.training;
+
+public class sumof100 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=0;
+int sum=0;
+while (i<=100)
+{
+	sum=sum+i;
+	i++;
+	}
+System.out.println(sum);
+	}
+
+}
