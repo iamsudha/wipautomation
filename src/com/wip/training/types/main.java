b7eeab111c5d3875c594048c69af0da5452794bc
Sudha Katta <sudhakatta@Sudhas-MacBook-Pro.local>
2017-02-13 21:44:34 -0800

    updated exercise 4 scripts

---
+++ src/com/wip/training/types/main.java
@@ -0,0 +1,15 @@
+package com.wip.training.types;
+
+public class main {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    rectangle rect = new rectangle(30,40);
+
+    //rect.length = 4;
+    rect.printDetails();
+
+  }
+
+
+}
---
+++ src/com/wip/training/types/rectangle.java
@@ -0,0 +1,28 @@
+package com.wip.training.types;
+
+public class rectangle {
+
+  private int length;
+  private int width;
+
+  public int area() {
+    return length * width;
+  }
+  public rectangle(int len,int wid){
+      length = len;
+      width =  wid;
+    
+   // len = length;
+    //wid = width;
+  }
+
+  public void printDetails() {
+    System.out.println("length of the rectangle is: " + length);
+    System.out.println("width of the rectangle is: " + width);
+  
+  }
+
+
+}
+
+
